58739ce12dccba5479da907ab34c93d0ba813665
JohanyF <johanyf627@gmail.com>
2023-01-16 21:54:53 -0600

    Added game function where is runs Rock-Paper-Scissors game.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice()
 {
     let choice = Math.floor((Math.random() * 3)+ 1);
@@ -19,7 +24,6 @@ function getComputerChoice()
     return ComputerChoice;
 }
 
-//console.log(getComputerChoice());
 
 function playRound(playerSelection, computerSelection)
 {
@@ -30,28 +34,71 @@ function playRound(playerSelection, computerSelection)
     }
     else if((playerSelection.toLowerCase() === "rock") && (computerSelection === "paper"))
     {
+        computerScore++;
         return "You Lose! Paper beats Rock";
+
     }
     else if((playerSelection.toLowerCase() === "rock") && (computerSelection === "scissors"))
     {
+        playerScore++;
         return "You Win! Rock beats Scissors";
+
+
     }
     else if((playerSelection.toLowerCase() === "paper") && (computerSelection === "rock"))
     {
+        playerScore++;
         return "You Win! Paper beats Rock";
+
     }
     else if((playerSelection.toLowerCase() === "paper") && (computerSelection === "scissors"))
     {
+        computerScore++;
         return "You Lose! Scissors beats Paper";
+
     }
     else if((playerSelection.toLowerCase() === "scissors") && (computerSelection === "rock"))
     {
+        computerScore++;
         return "You Lose! Rock beats Scissors";
+
     }
     else if((playerSelection.toLowerCase() === "scissors") && (computerSelection === "paper"))
     {
+        playerScore++;
         return "You Win! Scissors beats Paper";
+
     }
 }
 
-//console.log(playRound("Paper", "scissors"));
+//console.log(playerSelection);
+//console.log(computerSelection);
+//console.log(playRound(playerSelection, computerSelection));
+
+function game()
+{
+    
+    for(let i  = 0; i < 5; i++)
+    {
+        playerSelection = prompt("Rock, Paper or Scissors?")
+        computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if(playerScore > computerScore)
+    {
+        console.log("You Win! You beat the computer.");
+    }
+    else if(playerScore < computerScore)
+    {
+        console.log("You Lost! A computer is superior than you...");
+    }
+    else
+    {
+        console.log("Nobody won?! Tied Game... ");
+    }
+}
+
+game();
+console.log(playerScore)
+console.log(computerScore);
